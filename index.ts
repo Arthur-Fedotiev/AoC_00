48c68e715a86232549c25d094022c2154cf645f0
arturfedotiev <artur.fedotiew@gmail.com>
2022-12-03 06:38:12 +0100

    aoc_02_p1

--- index.ts
+++ index.ts
@@ -2,33 +2,68 @@ import { readFileSync } from "fs";
 
 const input = readFileSync("./input.txt", "utf-8");
 
-const sum = (arr: number[]) => arr.reduce((acc, curr) => acc + curr, 0);
-
-const mostCaloriesCarriedByTopThreeElves = (input: string): number[] => {
-  const elves = input.split(/\r?\n\r?\n/).map((elfStash) => {
-    return elfStash.split(/\r?\n/).map((item) => parseInt(item));
-  });
-
-  const mostCaloriesCarriedByTopThreeElves = elves.reduce((acc, curr) => {
-    const currCaloriesTotal = sum(curr);
-
-    if (acc.length < 3) {
-      acc.push(currCaloriesTotal);
-      return acc;
-    }
-
-    const lowestCalories = Math.min(...acc);
-
-    if (currCaloriesTotal > lowestCalories) {
-      acc.splice(acc.indexOf(lowestCalories), 1, currCaloriesTotal);
-    }
-
-    return acc;
-  }, []);
-
-  return mostCaloriesCarriedByTopThreeElves;
+const upperCharMap: Record<string, number> = {
+  A: 27,
+  B: 28,
+  C: 29,
+  D: 30,
+  E: 31,
+  F: 32,
+  G: 33,
+  H: 34,
+  I: 35,
+  J: 36,
+  K: 37,
+  L: 38,
+  M: 39,
+  N: 40,
+  O: 41,
+  P: 42,
+  Q: 43,
+  R: 44,
+  S: 45,
+  T: 46,
+  U: 47,
+  V: 48,
+  W: 49,
+  X: 50,
+  Y: 51,
+  Z: 52,
 };
 
-const result = sum(mostCaloriesCarriedByTopThreeElves(input));
+const lowerCharMap: Record<string, number> = Object.keys(upperCharMap).reduce(
+  (alphabet, curr) => ({
+    ...alphabet,
+    [curr.toLowerCase()]: upperCharMap[curr] - 26,
+  }),
+  {}
+);
 
-console.log(result);
+// console.log(lowerCharMap);
+
+const getRuckSaks = (input: string) => input.split(/\r?\n/g);
+const getCompartments = (rucksacks: string[]) =>
+  rucksacks.map((rucksack) => [
+    rucksack.slice(0, rucksack.length / 2),
+    rucksack.slice(rucksack.length / 2),
+  ]);
+
+const findDuplicatedItem = (compartment1: string, compartment2: string) =>
+  compartment1.split("").find((item1) => compartment2.includes(item1));
+
+const calcScoreOfDuplicatedItems = (duplicatedItems: string[]) =>
+  duplicatedItems.reduce(
+    (total, item) => (upperCharMap[item] ?? lowerCharMap[item]) + total,
+    0
+  );
+
+const compartments = getCompartments(getRuckSaks(input));
+const duplicatedItems = compartments.map(
+  ([comp1, comp2]) => findDuplicatedItem(comp1, comp2) as string
+);
+
+const result = calcScoreOfDuplicatedItems(duplicatedItems);
+
+console.log("getRuckSaks(input)", getRuckSaks(input)[1]);
+
+console.log("result", result);
